0a8f9a86efda74a534f0c30b565bbd8f49bf998b
bgarm <bgarm@yandex.ru>
2026-04-15 05:41:15 -0700

    feat: add Microsoft Clarity heatmap (wc2vxwrolh)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import LocalBusinessSchema from "@/components/LocalBusinessSchema";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -65,6 +66,19 @@ export default function RootLayout({
       className={`${inter.variable} ${sora.variable} h-full`}
     >
       <body className="min-h-full flex flex-col antialiased">
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wc2vxwrolh");
+            `,
+          }}
+        />
         <LocalBusinessSchema />
         <Header />
         <main className="flex-1">{children}</main>
